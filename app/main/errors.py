593127d8c012b16dd2b1f2f84ec5e2472c4ebbf0
sitrus <tomaszlaszczuk@gmail.com>
2015-03-24 11:09:08 +0100

    Custom error handlers views are now returning proper status code

--- app/main/errors.py
+++ app/main/errors.py
@@ -4,14 +4,14 @@ from . import main
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('_errors/404.html')
+    return render_template('_errors/404.html'), 404
 
 
 @main.app_errorhandler(500)
 def server_error(e):
-    return render_template('_errors/500.html')
+    return render_template('_errors/500.html'), 500
 
 
 @main.app_errorhandler(403)
 def forbidden(e):
-    return render_template('_errors/403.html')
+    return render_template('_errors/403.html'), 403
